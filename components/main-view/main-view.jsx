4e84901a438da57c522faf155350c99c46911dd5
Will D <pleplu01@gmail.com>
2023-06-14 19:04:14 -0400

    Integrated API into React project

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -1,38 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { MovieCard } from "../movie-card/movie-card";
 
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "The Godfather",
-      image: "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
-      description: "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.",
-      genre: "Crime",
-      director: "Francis Ford Coppola"
-    },
-    {
-      id: 2,
-      title: "Shawshank Redemption",
-      image: "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
-      description: "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.",
-      genre: "Drama",
-      director: "Frank Darabont"
-    },
-    {
-      id: 3,
-      title: "Schindler's List",
-      image: "https://upload.wikimedia.org/wikipedia/en/3/38/Schindler%27s_List_movie.jpg",
-      description: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
-      genre: "History",
-      director: "Steven Spielberg"
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://my-flix-8675.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const moviesFromApi = data.map((movie) => {
+          return {
+            _id: movie._id,
+            Title: movie.Title,
+            Description: movie.Description, 
+            ImagePath: movie.ImagePath,
+            Genre: movie.Genre,
+            Director: movie.Director
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
@@ -49,7 +44,7 @@ export const MainView = () => {
   return (
     <div>
       {movies.map((movie) => (
-        <MovieCard key={movie.id} 
+        <MovieCard key={movie._id} 
           movie={movie} 
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
